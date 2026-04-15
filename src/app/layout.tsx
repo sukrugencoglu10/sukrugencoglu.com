c356ea1c542a2c5b418ad9e1afdaf0a94dfd77ae
Şükrü Gençoğlu <sukrugencoglu10@gmail.com>
2026-04-15 17:29:41 +0300

    feat: setup Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,30 @@ export default async function RootLayout({
 
   return (
     <html lang={lang} className={`${inter.variable} ${signature.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KB82ZWW7');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="bg-white text-ink font-sans antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KB82ZWW7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
